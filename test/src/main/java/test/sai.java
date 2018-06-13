f13ce8060223d8b1f365e32b60f9fd86000e9dff
new <new@192.168.1.17>
2018-06-13 09:46:52 +0530

    aaa

--- test/src/main/java/test/sai.java
+++ test/src/main/java/test/sai.java
@@ -6,12 +6,15 @@ import org.testng.annotations.Test;
 
 public class sai {
 	@Test
-	public void abc(){
+	public void abcd(){
 		
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\new\\Downloads\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 	}
+	
+	
+	
 	
 
 }
